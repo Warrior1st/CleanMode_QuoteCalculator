da1964d6e0ada0aea4ae56e4a183eed26c4e05ca
Warrior1st <61919684+Warrior1st@users.noreply.github.com>
2021-03-30 23:28:25 -0400

    Initial layout to make the calculator

--- CleanMode_QuoteCalculator/wwwroot/js/site.js
+++ CleanMode_QuoteCalculator/wwwroot/js/site.js
@@ -1,4 +1,13 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+$(document).ready(function () {
+    $('#first').click(function () {
+        $('#location').fadeOut();
+        $('#units').slideUp;
+        
+    });
+    
+
+});
 // Write your JavaScript code.
